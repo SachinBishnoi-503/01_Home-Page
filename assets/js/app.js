a69c5b95006d11b9e8dfa51c3e9c8ae4bd1679c8
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-04-26 12:04:52 +0530

    slick slider section completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,54 @@ let menulist = document.querySelector(".nav-items");
 menuicon.addEventListener("click", function () {
     menulist.classList.toggle("show");
 })
+$('.sliderbox').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  arrow: true,
+   nextArrow: ".next",
+    prevArrow: ".prev",
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1170,
+      settings: {
+        slidesToShow: 3,
+        
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+       {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    
+    
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 567,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
