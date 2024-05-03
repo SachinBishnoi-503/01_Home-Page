6be67584e96d2dcbbd723977ebb1452f2e3d1b5c
SachinBishnoi-503 <bishnoisachin626@gmail.com>
2024-05-03 10:13:58 +0530

    back to top button

--- assets/js/app.js
+++ assets/js/app.js
@@ -55,3 +55,20 @@ $('.sliderbox').slick({
     // instead of a settings object
   ]
 });
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
